4e0c650f5cb44700bc9cd1123f7e70c063a0297f
DMAndy <andy@digitalmarmalade.co.uk>
2015-03-05 13:02:28 +0000

    add deepcopy function

---
+++ functions/function.deepcopy.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copy a file, or recursively copy a folder and its contents
+ * @param       string   $source    Source path
+ * @param       string   $dest      Destination path
+ * @param       string   $permissions New folder creation permissions
+ * @return      bool     Returns true on success, false on failure
+ */
+function deepcopy($source, $dest, $permissions = 0755)
+{
+    // Check for symlinks
+    if (is_link($source)) {
+        return symlink(readlink($source), $dest);
+    }
+
+    // Simple copy for a file
+    if (is_file($source)) {
+        return copy($source, $dest);
+    }
+
+    // Make destination directory
+    if (!is_dir($dest)) {
+        mkdir($dest, $permissions);
+    }
+
+    // Loop through the folder
+    $dir = dir($source);
+    while (false !== $entry = $dir->read()) {
+        // Skip pointers
+        if ($entry == '.' || $entry == '..') {
+            continue;
+        }
+
+        // Deep copy directories
+        deepcopy("$source/$entry", "$dest/$entry", $permissions);
+    }
+
+    // Clean up
+    $dir->close();
+    return true;
+
+}
+
+?>
