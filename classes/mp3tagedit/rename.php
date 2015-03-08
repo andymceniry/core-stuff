ab7eb925eca269b974c5d43c966d53f1ea5e0d0c
Andy McEniry <andy@mceniry.me.uk>
2015-03-08 11:32:58 +0000

    add new function

--- classes/mp3tagedit/rename.php
+++ classes/mp3tagedit/rename.php
@@ -7,12 +7,23 @@ $oTag = new mp3tagedit();
 $oTag->path = 'tracks';
 
 $oTag->defaultTags = array(
-    'artist' => 'Artist Name Tag',
-    'album' => 'Album Name Tag',
-    'year' => '1234'
+    'artist' => 'Sante Les Amis',
+    'album' => 'Sudamericana',
+    'year' => '2012'
 );
 
-$oTag->addTrackTags('existing_filename', array('title' => 'Track Title', 'artist' => array('Artist')));
+$oTag->addTrackTags('z-00', array('title' => 'El Rayo, El Trueno Y El Silbido'));
+$oTag->addTrackTags('z-01', array('title' => 'The Byte Of Love'));
+$oTag->addTrackTags('z-02', array('title' => 'Brasil'));
+$oTag->addTrackTags('z-03', array('title' => 'Huracan'));
+$oTag->addTrackTags('z-04', array('title' => 'Robot'));
+$oTag->addTrackTags('z-06', array('title' => 'El Ultimo Verano'));
+$oTag->addTrackTags('z-07', array('title' => 'The Road'));
+$oTag->addTrackTags('z-08', array('title' => 'She Gets Me Excited'));
+$oTag->addTrackTags('z-09', array('title' => 'Dormido'));
+$oTag->addTrackTags('z-11', array('title' => 'The River'));
+$oTag->addTrackTags('z-12', array('title' => 'Black Rain'));
+
 
 $oTag->updateTracks();
 ?>
